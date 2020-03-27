0541d845bce99bda9621334667bfbb99f98da7df
mehmetsan <trabzonpower@gmail.com>
2020-03-27 12:13:55 +0300

    PopUp

--- androidTest.py
+++ androidTest.py
@@ -12,3 +12,22 @@ driver = webdriver.Remote("http://localhost:4723/wd/hub",desired_cap)
 
 button = driver.find_element_by_id("com.sand.airmirror:id/tvLogin")
 button.click()
+
+driver.implicitly_wait(2)
+
+email = driver.find_element_by_id("com.sand.airmirror:id/etAccount")
+email = email.find_element_by_class_name("android.widget.EditText")
+email.send_keys(("trabzonpower@gmail.com"))
+
+password = driver.find_element_by_id("com.sand.airmirror:id/etPwd")
+password = password.find_element_by_class_name("android.widget.EditText")
+password.send_keys("northern61")
+
+btn = driver.find_element_by_id("com.sand.airmirror:id/btnLogin")
+btn.click()
+
+driver.implicitly_wait(4)
+
+
+popUp = driver.find_element_by_id("com.sand.airmirror:id/tvCancel")
+popUp.click()
